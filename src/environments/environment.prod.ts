fd4c5412e4c68a1085554cebd5bc3f8edc0fb1d0
AntiJcup <jacobkyle99@yahoo.com>
2019-12-06 09:47:26 -0800

    Add staging config to angular json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   apiHost: 'https://api.tutorbits.com',
-  contentHost: 'https://content.tutorbits.com',
   // tslint:disable-next-line: max-line-length
   loginUrl: 'https://login.tutorbits.com/oauth2/authorize?client_id=60bttusi8qioo53pfnqtgqcn4s&response_type=code&scope=email+openid+aws.cognito.signin.user.admin&redirect_uri=https://www.tutorbits.com/login',
   logoutUrl: 'https://login.tutorbits.com/logout?client_id=60bttusi8qioo53pfnqtgqcn4s&logout_uri=https://www.tutorbits.com/logout',
