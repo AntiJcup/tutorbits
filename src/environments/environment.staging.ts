153cacfcd6cf1ebdc1e415dc3d5f5c4cc300bb75
AntiJcup <jacobkyle99@yahoo.com>
2019-12-06 09:34:37 -0800

    setup staging environment add prod main workflow

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  apiHost: 'https://api.staging.tutorbits.com',
+  contentHost: 'https://content.staging.tutorbits.com',
+  // tslint:disable-next-line: max-line-length
+  loginUrl: 'https://login.tutorbits.com/oauth2/authorize?client_id=60bttusi8qioo53pfnqtgqcn4s&response_type=code&scope=email+openid+aws.cognito.signin.user.admin&redirect_uri=https://www.staging.tutorbits.com/login',
+  logoutUrl: 'https://login.tutorbits.com/logout?client_id=60bttusi8qioo53pfnqtgqcn4s&logout_uri=https://www.staging.tutorbits.com/logout',
+  loginTokenUrl: 'https://login.tutorbits.com/oauth2/token',
+  loginClientId: '60bttusi8qioo53pfnqtgqcn4s',
+  loginRedirectUri: 'https://www.staging.tutorbits.com/login',
+  logoutRedirectUri: 'https://www.staging.tutorbits.com/logout',
+  loggingEnabled: true,
+  loggingTraceEnabled: false
+};
