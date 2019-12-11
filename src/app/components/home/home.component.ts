5419a9fba889b7cfdd50e596bcc1ebad8dc5e35d
AntiJcup <jacobkyle99@yahoo.com>
2019-12-11 09:49:06 -0800

    Update initial text

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,7 +8,7 @@ import { Article } from 'src/app/models/blog/article';
 export class HomeComponent implements OnInit {
   articles: Article[] = [{
     title: 'Welcome to TutorBits!',
-    body: 'I am very happy to share with you my most recent project. This website is designed and made to help teach programming in a more integrated fashion.\n\n\n How you may ask? Programming tutorials in the form of videos are currently flat. With this website you get to experience the tutorial video and the teachers editor as it is being written. Want to test the code the teacher has written at a particular spot? Click preview and test it. Want to take what the teacher has made and make your own modifications? Export to sandbox and edit to your hearts content. \n\n\n Start by clicking the Tutorials button in the top right. \n Thanks for visiting!',
+    body: 'This website is designed to help teach programming by example while being able to tinker with the code as you go. \nWatch pros program all kinds of projects all the way through learning the process of making the project. Then tinker with the project in the sandbox adding your own features.\n\n Start by clicking the Tutorials button in the top right.\nThanks for visiting!',
     owner: 'Jcup'
   } as Article];
   constructor() { }
