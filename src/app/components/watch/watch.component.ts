85c83d5bad91f5206444ce716f9ada1adc4192d8
AntiJcup <jacobkyle99@yahoo.com>
2019-12-27 14:32:30 -0800

    Change edit in sandbox to better present choices

--- src/app/components/watch/watch.component.ts
+++ src/app/components/watch/watch.component.ts
@@ -249,10 +249,10 @@ export class WatchComponent implements OnInit, OnDestroy {
     });
   }
 
-  public onCopyToSandboxClicked(e: any) {
+  public onCopyToSandboxClicked(e: any, newWindow: boolean) {
     this.eventService.TriggerButtonClick('Watch', `Sandbox - ${this.projectId}`);
     // this.router.navigate([`sandbox/${this.projectId}`]);
-    if (confirm(`Click "Ok" to open the sandbox in a new window or "Cancel" in a newtab`)) {
+    if (newWindow) {
       window.open(`sandbox/${this.projectId}`, 'Sandbox', 'height=720,width=1080');
     } else {
       window.open(`sandbox/${this.projectId}`);
