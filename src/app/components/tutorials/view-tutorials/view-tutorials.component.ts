4e07bd3a435801672e4deb948532c3f6443760ec
AntiJcup <jacobkyle99@yahoo.com>
2020-01-01 06:23:19 -0800

    Add meta description to the tutorials page

--- src/app/components/tutorials/view-tutorials/view-tutorials.component.ts
+++ src/app/components/tutorials/view-tutorials/view-tutorials.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ViewTutorial } from 'src/app/models/tutorial/view-tutorial';
 import { Router } from '@angular/router';
 import { TutorBitsTutorialService } from 'src/app/services/tutor-bits-tutorial.service';
 import { IErrorService } from 'src/app/services/abstract/IErrorService';
 import { ILogService } from 'src/app/services/abstract/ILogService';
 import { ITitleService } from 'src/app/services/abstract/ITitleService';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   templateUrl: './view-tutorials.component.html',
   styleUrls: ['./view-tutorials.component.sass']
 })
-export class ViewTutorialsComponent implements OnInit {
+export class ViewTutorialsComponent implements OnInit, OnDestroy {
   tutorials: Array<ViewTutorial> = [];
   tutorialsByType: { [key: string]: ViewTutorial[] } = {};
   tutorialTypes: string[] = [];
@@ -22,10 +23,16 @@ export class ViewTutorialsComponent implements OnInit {
     private tutorialsService: TutorBitsTutorialService,
     private errorServer: IErrorService,
     private logServer: ILogService,
-    private titleService: ITitleService) { }
+    private titleService: ITitleService,
+    private metaService: Meta) { }
 
   ngOnInit() {
-    this.titleService.SetTitle('Gallery');
+    this.titleService.SetTitle('TutorBits - Gallery');
+    this.metaService.updateTag({
+      name: 'description',
+      content: `TutorBits - Home to a better programming tutorial experience. Watch as programmers write code, interact, and test the code as they write it.`
+    },
+      'name=\'description\'');
     this.tutorialsService.GetAll().then((tutorials) => {
       this.tutorials = tutorials;
       this.logServer.LogToConsole('ViewTutorials', tutorials.length);
@@ -54,4 +61,7 @@ export class ViewTutorialsComponent implements OnInit {
     this.router.navigate([`watch/${tutorial.id}/${tutorial.title}`]);
   }
 
+  ngOnDestroy(): void {
+    this.metaService.removeTag('name=\'description\'');
+  }
 }
