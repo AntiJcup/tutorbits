d885de4ad572313f794e3daee7c92265e3606a33
AntiJcup <jacobkyle99@yahoo.com>
2020-01-01 07:31:14 -0800

    Add type to title of watch

--- src/app/components/watch/watch.component.ts
+++ src/app/components/watch/watch.component.ts
@@ -101,7 +101,7 @@ export class WatchComponent implements OnInit, OnDestroy {
     }
 
     this.tutorialService.Get(this.projectId).then((tutorial: ViewTutorial) => {
-      this.titleService.SetTitle(`${tutorial.title} - TutorBits Tutorial`);
+      this.titleService.SetTitle(`${tutorial.title} - ${tutorial.type} Tutorial - TutorBits`);
       this.metaService.updateTag({ name: 'description', content: `TutotorBits Tutorial - ${tutorial.title}: ${tutorial.description}` },
         'name=\'description\'');
     });
