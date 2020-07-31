f761582e6d2d5327a62fcf66008795bd3af92a3a
AntiJcup <jacobkyle99@yahoo.com>
2020-07-31 11:58:57 -0700

    Work in progress python language support

---
+++ shared/web/lib/ts/RespondingWebsocket.ts
@@ -0,0 +1,68 @@
+import ReconnectingWebSocket, { UrlProvider, Options } from 'reconnecting-websocket';
+
+// Assumes event data is a json string
+// tslint:disable-next-line: max-line-length
+export class RespondingWebSocket<TResponseMessageInterface, ResponseIdField extends keyof TResponseMessageInterface> extends ReconnectingWebSocket {
+  private responseCallback: (event: MessageEvent) => void = null;
+  private expectedResponses: Map<TResponseMessageInterface[ResponseIdField], (TResponseMessageInterface) => void> =
+    new Map<TResponseMessageInterface[ResponseIdField], (TResponseMessageInterface) => void>();
+
+  constructor(private idField: ResponseIdField, url: UrlProvider, protocols?: string | string[], options?: Options) {
+    super(url, protocols, options);
+  }
+
+  private setupListener() {
+    if (this.responseCallback) {
+      return;
+    }
+
+    this.responseCallback = (event) => { this.onResponse(event); };
+    this.addEventListener('message', this.responseCallback);
+  }
+
+  private onResponse(event: MessageEvent) {
+    if (this.expectedResponses.size <= 0) {
+      return;
+    }
+
+    const parsedData = JSON.parse(event.data) as TResponseMessageInterface;
+    let matchedId: TResponseMessageInterface[ResponseIdField];
+    for (const expectedId of this.expectedResponses.keys()) {
+      const id = parsedData[this.idField];
+      if (id !== expectedId) {
+        continue;
+      }
+
+      matchedId = expectedId;
+    }
+
+    if (!matchedId) {
+      return;
+    }
+
+    const response = this.expectedResponses.get(matchedId);
+    this.expectedResponses.delete(matchedId);
+    setTimeout(() => response(parsedData), 0);
+  }
+
+  public async listenForResponse(expectedValue: TResponseMessageInterface[ResponseIdField])
+    : Promise<TResponseMessageInterface> {
+    this.setupListener();
+
+    let responseFinishCallback: (TResponseMessageInterface) => void;
+    // tslint:disable-next-line: no-shadowed-variable
+    const response = new Promise<TResponseMessageInterface>((resolve) => {
+      responseFinishCallback = resolve;
+    });
+
+    this.expectedResponses.set(expectedValue, responseFinishCallback);
+
+    return response;
+  }
+
+  public dispose() {
+    if (this.responseCallback) {
+      this.removeEventListener('message', this.responseCallback);
+    }
+  }
+}
--- src/app/sub-components/editors/editor/monaco-editor.component.ts
+++ src/app/sub-components/editors/editor/monaco-editor.component.ts
@@ -1,12 +1,160 @@
 import { Output, EventEmitter, OnDestroy, Directive } from '@angular/core';
 import { ILogService } from 'src/app/services/abstract/ILogService';
+import { MonacoToProtocolConverter, ProtocolToMonacoConverter } from 'monaco-languageclient/lib/monaco-converter';
+import { RespondingWebSocket } from 'shared/web/lib/ts/RespondingWebSocket';
+import * as normalizeUrl from 'normalize-url';
+import { Guid } from 'guid-typescript';
+import { CompletionItemKind, SymbolKind, CompletionItem } from 'monaco-languageclient/lib/services';
 
 export interface GoToDefinitionEvent {
   path: string;
   offset: monaco.Position;
 }
 
+// TODO MOVE THIS SHIT
+const PYTHON_LANGUAGE_ID = 'python';
+
+export enum CommandType {
+  Arguments,
+  Completions,
+  Hover,
+  Usages,
+  Definitions,
+  Symbols
+}
+
+const commandNames = new Map<CommandType, string>();
+commandNames.set(CommandType.Arguments, 'arguments');
+commandNames.set(CommandType.Completions, 'completions');
+commandNames.set(CommandType.Definitions, 'definitions');
+commandNames.set(CommandType.Hover, 'tooltip');
+commandNames.set(CommandType.Usages, 'usages');
+commandNames.set(CommandType.Symbols, 'names');
+
+// tslint:disable-next-line:no-unused-variable
+export interface ICommand {
+  id: string;
+  telemetryEvent?: string;
+  command: CommandType;
+  source?: string;
+  fileName: string;
+  lineIndex: number;
+  columnIndex: number;
+}
+
+export interface IResponse {
+  id: string;
+  result: any;
+}
+
+export interface IAutoCompleteItem {
+  type: CompletionItemKind;
+  rawType: CompletionItemKind;
+  kind: SymbolKind;
+  text: string;
+  description: string;
+  raw_docstring: string;
+  rightLabel: string;
+}
+
+export interface ICompletionResult {
+  items: IAutoCompleteItem[];
+}
+
+const pythonVSCodeTypeMappings = new Map<string, CompletionItemKind>();
+pythonVSCodeTypeMappings.set('none', CompletionItemKind.Value);
+pythonVSCodeTypeMappings.set('type', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('tuple', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('dict', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('dictionary', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('function', CompletionItemKind.Function);
+pythonVSCodeTypeMappings.set('lambda', CompletionItemKind.Function);
+pythonVSCodeTypeMappings.set('generator', CompletionItemKind.Function);
+pythonVSCodeTypeMappings.set('class', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('instance', CompletionItemKind.Reference);
+pythonVSCodeTypeMappings.set('method', CompletionItemKind.Method);
+pythonVSCodeTypeMappings.set('builtin', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('builtinfunction', CompletionItemKind.Function);
+pythonVSCodeTypeMappings.set('module', CompletionItemKind.Module);
+pythonVSCodeTypeMappings.set('file', CompletionItemKind.File);
+pythonVSCodeTypeMappings.set('xrange', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('slice', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('traceback', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('frame', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('buffer', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('dictproxy', CompletionItemKind.Class);
+pythonVSCodeTypeMappings.set('funcdef', CompletionItemKind.Function);
+pythonVSCodeTypeMappings.set('property', CompletionItemKind.Property);
+pythonVSCodeTypeMappings.set('import', CompletionItemKind.Module);
+pythonVSCodeTypeMappings.set('keyword', CompletionItemKind.Keyword);
+pythonVSCodeTypeMappings.set('constant', CompletionItemKind.Variable);
+pythonVSCodeTypeMappings.set('variable', CompletionItemKind.Variable);
+pythonVSCodeTypeMappings.set('value', CompletionItemKind.Value);
+pythonVSCodeTypeMappings.set('param', CompletionItemKind.Variable);
+pythonVSCodeTypeMappings.set('statement', CompletionItemKind.Keyword);
+
+const pythonVSCodeSymbolMappings = new Map<string, SymbolKind>();
+pythonVSCodeSymbolMappings.set('none', SymbolKind.Variable);
+pythonVSCodeSymbolMappings.set('type', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('tuple', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('dict', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('dictionary', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('function', SymbolKind.Function);
+pythonVSCodeSymbolMappings.set('lambda', SymbolKind.Function);
+pythonVSCodeSymbolMappings.set('generator', SymbolKind.Function);
+pythonVSCodeSymbolMappings.set('class', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('instance', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('method', SymbolKind.Method);
+pythonVSCodeSymbolMappings.set('builtin', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('builtinfunction', SymbolKind.Function);
+pythonVSCodeSymbolMappings.set('module', SymbolKind.Module);
+pythonVSCodeSymbolMappings.set('file', SymbolKind.File);
+pythonVSCodeSymbolMappings.set('xrange', SymbolKind.Array);
+pythonVSCodeSymbolMappings.set('slice', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('traceback', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('frame', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('buffer', SymbolKind.Array);
+pythonVSCodeSymbolMappings.set('dictproxy', SymbolKind.Class);
+pythonVSCodeSymbolMappings.set('funcdef', SymbolKind.Function);
+pythonVSCodeSymbolMappings.set('property', SymbolKind.Property);
+pythonVSCodeSymbolMappings.set('import', SymbolKind.Module);
+pythonVSCodeSymbolMappings.set('keyword', SymbolKind.Variable);
+pythonVSCodeSymbolMappings.set('constant', SymbolKind.Constant);
+pythonVSCodeSymbolMappings.set('variable', SymbolKind.Variable);
+pythonVSCodeSymbolMappings.set('value', SymbolKind.Variable);
+pythonVSCodeSymbolMappings.set('param', SymbolKind.Variable);
+pythonVSCodeSymbolMappings.set('statement', SymbolKind.Variable);
+pythonVSCodeSymbolMappings.set('boolean', SymbolKind.Boolean);
+pythonVSCodeSymbolMappings.set('int', SymbolKind.Number);
+pythonVSCodeSymbolMappings.set('longlean', SymbolKind.Number);
+pythonVSCodeSymbolMappings.set('float', SymbolKind.Number);
+pythonVSCodeSymbolMappings.set('complex', SymbolKind.Number);
+pythonVSCodeSymbolMappings.set('string', SymbolKind.String);
+pythonVSCodeSymbolMappings.set('unicode', SymbolKind.String);
+pythonVSCodeSymbolMappings.set('list', SymbolKind.Array);
+
+function getMappedVSCodeType(pythonType: string): CompletionItemKind {
+  if (pythonVSCodeTypeMappings.has(pythonType)) {
+    const value = pythonVSCodeTypeMappings.get(pythonType);
+    if (value) {
+      return value;
+    }
+  }
+  return CompletionItemKind.Keyword;
+}
+
+function getMappedVSCodeSymbol(pythonType: string): SymbolKind {
+  if (pythonVSCodeSymbolMappings.has(pythonType)) {
+    const value = pythonVSCodeSymbolMappings.get(pythonType);
+    if (value) {
+      return value;
+    }
+  }
+  return SymbolKind.Variable;
+}
+
 @Directive()
+// tslint:disable-next-line: directive-class-suffix
 export abstract class MonacoEditorComponent implements OnDestroy {
   private static editOptions: monaco.editor.IEditorOptions = {
     readOnly: false
@@ -23,6 +171,9 @@ export abstract class MonacoEditorComponent implements OnDestroy {
   private filePath: string;
   private ignoreNext = false;
   public visible = false;
+
+  private m2p = new MonacoToProtocolConverter();
+  private p2m = new ProtocolToMonacoConverter();
 
   public get ignoreNextEvent(): boolean {
     const res = this.ignoreNext;
@@ -78,6 +229,8 @@ export abstract class MonacoEditorComponent implements OnDestroy {
       this.gotoDefinition.emit(
         { path: file, offset: new monaco.Position(range.startLineNumber, range.startColumn) } as GoToDefinitionEvent);
     };
+
+    this.setupPython();
   }
 
   public get currentFilePath() {
@@ -201,5 +354,104 @@ export abstract class MonacoEditorComponent implements OnDestroy {
   public GenerateNewEditorModel(path: string, data: string = ''): monaco.editor.ITextModel {
     this.logServer.LogToConsole('editor', `Generating new model: ${path}`);
     return monaco.editor.createModel(data, this.GetLanguageByPath(path), monaco.Uri.file(path));
+  }
+
+  private createUrl(path: string): string {
+    const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+    return normalizeUrl(`${protocol}://${location.host}${location.pathname}${path}`);
+  }
+
+  private createWebSocket(url: string): RespondingWebSocket<IResponse, 'id'> {
+    const socketOptions = {
+      maxReconnectionDelay: 10000,
+      minReconnectionDelay: 1000,
+      reconnectionDelayGrowFactor: 1.3,
+      connectionTimeout: 10000,
+      maxRetries: Infinity,
+      debug: false
+    };
+    return new RespondingWebSocket('id', url, [], socketOptions);
+  }
+
+
+  private setupPython() {
+    const url = this.createUrl('/');
+    const webSocket = this.createWebSocket(url);
+
+    webSocket.onopen = () => {
+      monaco.languages.register({
+        id: PYTHON_LANGUAGE_ID,
+        extensions: ['.py'],
+        aliases: [PYTHON_LANGUAGE_ID],
+        mimetypes: ['application/python'],
+      });
+
+      monaco.languages.registerCompletionItemProvider(PYTHON_LANGUAGE_ID, {
+        async provideCompletionItems(model, position, context, token): Promise<monaco.languages.CompletionList> {
+          const type = CommandType.Completions;
+          const columnIndex = position.column;
+
+          const source = model.getValue();
+          const cmd: ICommand = {
+            id: Guid.create().toString(), // todo generate in  a more common spot
+            command: type,
+            fileName: model.uri.toString(),
+            columnIndex,
+            lineIndex: position.lineNumber,
+            source
+          };
+
+          const payload = {
+            id: Guid.create(), // todo generate in  a more common spot
+            prefix: '',
+            lookup: commandNames.get(cmd.command),
+            path: cmd.fileName,
+            source: cmd.source,
+            line: cmd.lineIndex,
+            column: cmd.columnIndex,
+            config: this.getConfig()
+          };
+
+          webSocket.send(JSON.stringify(cmd));
+          const response = await webSocket.listenForResponse(cmd.id);
+          let results = response.result as IAutoCompleteItem[];
+
+          results = Array.isArray(results) ? results : [];
+          results.forEach((item) => {
+            // tslint:disable-next-line:no-any
+            const originalType = (item.type as any) as string;
+            item.type = getMappedVSCodeType(originalType);
+            item.kind = getMappedVSCodeSymbol(originalType);
+            item.rawType = getMappedVSCodeType(originalType);
+          });
+          const completionResult: ICompletionResult = {
+            items: results
+          };
+
+          return {
+            suggestions: completionResult.items.map((item) => {
+              const completionItem: monaco.languages.CompletionItem = {
+                label: item.text,
+                kind: item.type,
+                range: null,
+                insertText: null
+              };
+
+              if (item.kind === SymbolKind.Function || item.kind === SymbolKind.Method) {
+                completionItem.insertText = `${item.text}()`;
+              }
+
+              return completionItem;
+            })
+          } as monaco.languages.CompletionList;
+        },
+
+        resolveCompletionItem(model, position, item, token):
+          monaco.languages.CompletionItem | monaco.Thenable<monaco.languages.CompletionItem> {
+          // return jsonService.doResolve(m2p.asCompletionItem(item)).then(result => p2m.asCompletionItem(result, item.range));
+          return null as monaco.languages.CompletionItem;
+        }
+      });
+    };
   }
 }
